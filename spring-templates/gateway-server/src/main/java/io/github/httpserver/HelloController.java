137de0e20a460ff0e239840865c274e43f241bc5
Lucas Song <yuanzheng@cs.byu.edu>
2021-04-13 17:30:55 +0800

    Demo 1, A http server via SpringBoot

---
+++ spring-templates/gateway-server/src/main/java/io/github/httpserver/HelloController.java
@@ -0,0 +1,20 @@
+
+
+package io.github.httpserver;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String sayHello(HttpServletRequest request) {
+//        String code = request.getHeader("exchange-cloud");
+        return "hello world";
+    }
+}
