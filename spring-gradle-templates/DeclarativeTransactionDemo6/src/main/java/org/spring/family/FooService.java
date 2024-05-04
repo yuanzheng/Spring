35af0c93bf3cb04f765093f77a2f5cf37080d510
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2024-05-04 11:25:36 +0800

    Chapter3, declarative transaction demo6

---
+++ spring-gradle-templates/DeclarativeTransactionDemo6/src/main/java/org/spring/family/FooService.java
@@ -0,0 +1,7 @@
+package org.spring.family;
+
+public interface FooService {
+    void insertRecord();
+    void insertThenRollback() throws RollbackException;
+    void invokeInsertThenRollback() throws RollbackException;
+}
---
+++ spring-gradle-templates/DeclarativeTransactionDemo6/src/main/java/org/spring/family/FooServiceImpl.java
@@ -0,0 +1,30 @@
+package org.spring.family;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class FooServiceImpl implements FooService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    @Transactional
+    public void insertRecord() {
+        jdbcTemplate.execute("INSERT INTO FOO (BAR) VALUES ('AAA')");
+    }
+
+    @Override
+    @Transactional(rollbackFor = RollbackException.class)
+    public void insertThenRollback() throws RollbackException {
+        jdbcTemplate.execute("INSERT INTO FOO (BAR) VALUES ('BBB')");
+        throw new RollbackException();
+    }
+
+    @Override
+    public void invokeInsertThenRollback() throws RollbackException {
+        insertThenRollback();
+    }
+}
