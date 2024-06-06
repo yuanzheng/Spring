0e0c1578748552f30871962dbbcb334fa8939e39
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2024-06-06 17:09:29 +0800

    Add a gradle project to demo redis Jedis connection

---
+++ spring-gradle-templates/redis-jedis-gradle/src/main/java/geektime/spring/springbucks/config/AppConfig.java
@@ -0,0 +1,23 @@
+package geektime.spring.springbucks.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    @ConfigurationProperties("redis")
+    public JedisPoolConfig jedisPoolConfig() {
+        return new JedisPoolConfig();
+    }
+
+    @Bean(destroyMethod = "close")
+    public JedisPool jedisPool(@Value("${redis.host}") String host) {
+        return new JedisPool(jedisPoolConfig(), host);
+    }
+}
